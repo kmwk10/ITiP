f5aca7586a43346f38661a0c06787ce732a200b7
kk10 <kamy.zero.10@gmail.com>
2024-09-11 19:26:56 +0300

    LR1

---
+++ LR1/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            System.out.println(isPalindrome(s)); 
+        }
+    } 
+    public static String reverseString(String s) {
+        String reversed = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+        return reversed;
+    }
+    public static boolean isPalindrome(String s) {
+        if (s.equals(reverseString(s))) {
+            return true;
+        }
+        return false;
+    }
+}
